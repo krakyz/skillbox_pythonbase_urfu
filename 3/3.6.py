06d054d13e491924d983671dcc0e5536d262dd6c
Stepan Surkov <66885212+krakyz@users.noreply.github.com>
2022-09-29 20:31:09 +0500

    Update and rename 3.6.py to 3_06.py

--- 3/3.6.py
+++
@@ -1,7 +0,0 @@
-fisrt_num = int(input('Введите первое число: '))
-second_num = int(input('Введите второе число: '))
-
-first_digits = fisrt_num % 100
-second_digits = second_num % 100
-
-print('Сумма:', first_digits + second_digits)
